6e1a234e2d94787113cdc4ed2d063ea7d4ab65ad
cc <cc@dev.com>
2019-08-08 09:00:31 +0800

    Initial application generated by JHipster-6.2.0

---
+++ src/main/java/com/cc/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.cc.repository;
+
+import com.cc.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
